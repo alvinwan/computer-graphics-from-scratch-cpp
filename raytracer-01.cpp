33a97eca5e64ac3ec1429576acc73783cb783874
Alvin Wan <alvinwan1@gmail.com>
2024-02-09 15:00:03 +0800

    fix memory leak + add typedef for simpler syntax

--- raytracer-01.cpp
+++ raytracer-01.cpp
@@ -13,9 +13,11 @@ Implementation for https://gabrielgambetta.com/computer-graphics-from-scratch/02
 */
 #include "bmp.h"
 #include <math.h>
+#include <array>
 
-const float INF = std::numeric_limits<float>::infinity();
-const uint8_t BACKGROUND_COLOR[3] = {255, 255, 255};
+typedef std::array<float, 3> float3;
+typedef std::array<uint8_t, 3> rgb;
+const rgb BACKGROUND_COLOR = {255, 255, 255};
 
 // Canvas
 
@@ -25,7 +27,7 @@ bool put_pixel(
     int32_t height,
     int32_t x,
     int32_t y,
-    const uint8_t color[3]
+    const rgb color
 ) {
     // Translate [-W/2, W/2] into [0, W]. Ditto for H
     x = width / 2 + x;
@@ -48,54 +50,45 @@ bool put_pixel(
 // Linear Algebra
 
 // Compute dot product between two 3d vectors
-float dot(float v1[3], float v2[3]) {
+float dot(float3 v1, float3 v2) {
     return v1[0] * v2[0] + v1[1] * v2[1] + v1[2] * v2[2];
 }
 
 // Elementwise subtraction between two 3d vectors
-float* subtract(float v1[3], float v2[3]) {
-    float* point = new float[3];
-    point[0] = v1[0] - v2[0];
-    point[1] = v1[1] - v2[1];
-    point[2] = v1[2] - v2[2];
-    return point;
+float3 subtract(float3 a, float3 b) {
+    return {a[0] - b[0], a[1] - b[1], a[2] - b[2]};
 }
 
 // Ray tracing
 
 struct Sphere {
-    float center[3];
+    float3 center;
     float radius;
-    uint8_t color[3];
+    rgb color;
 
     Sphere() {}
 
-    Sphere(const std::initializer_list<float>& c, float r, const std::initializer_list<uint8_t>& col) {
-        std::copy(c.begin(), c.end(), center);
-        radius = r;
-        std::copy(col.begin(), col.end(), color);
+    Sphere(const float3& v_center, float v_radius, const rgb& v_color) {
+        center = v_center;
+        radius = v_radius;
+        color = v_color;
     }
 };
 
 // Convert 2d pixel coordinates to 3d viewport coordinates.
-float* canvas_to_viewport(int32_t x, int32_t y, int32_t width, int32_t height) {
-    float* point = new float[3];
-    point[0] = (float) x / width;
-    point[1] = (float) y / height;
-    point[2] = 1;
-    return point;
+float3 canvas_to_viewport(int32_t x, int32_t y, int32_t width, int32_t height) {
+    return { (float) x / width, (float) y / height, 1 };
 }
 
 
 // Computes intersection of ray with spheres. Returns solutions in terms of
 // line parameter t.
-float* intersect_ray_with_sphere(
-    float origin[3],
-    float direction[3],
+std::tuple<float, float> intersect_ray_with_sphere(
+    float3 origin,
+    float3 direction,
     Sphere sphere
 ) {
-    float* out = new float[2];
-    float* center = subtract(origin, sphere.center);
+    float3 center = subtract(origin, sphere.center);
 
     float a = dot(direction, direction);
     float b = 2 * dot(center, direction);
@@ -103,44 +96,45 @@ float* intersect_ray_with_sphere(
 
     float discriminant = b * b - 4 * a * c;
     if (discriminant > 0) {
-        out[0] = (-b + sqrt(discriminant)) / (2 * a);
-        out[1] = (-b - sqrt(discriminant)) / (2 * a);
-    } else {
-        out[0] = INF;
-        out[1] = INF;
+        return std::make_tuple(
+            (-b + sqrt(discriminant)) / (2 * a),
+            (-b - sqrt(discriminant)) / (2 * a)
+        );
     }
-    return out;
+    return std::make_tuple(INFINITY, INFINITY);
 }
 
 // Traces a ray against the spheres in the scene
-const uint8_t* trace_ray(
-    float origin[3],
-    float direction[3],
+rgb trace_ray(
+    float3 origin,
+    float3 direction,
     float min_t,
     float max_t,
     Sphere spheres[],
     int32_t num_spheres
 ) {
-    float closest_t = INF;
+    float closest_t = INFINITY;
     Sphere closest_sphere;
 
     for (int i = 0; i < num_spheres; i++) {
-        float* ts = intersect_ray_with_sphere(origin, direction, spheres[i]);
-        if (ts[0] < closest_t && min_t < ts[0] && ts[0] < max_t) {
-            closest_t = ts[0];
+        std::tuple<float, float> ts = intersect_ray_with_sphere(origin, direction, spheres[i]);
+        float t0 = std::get<0>(ts);
+        float t1 = std::get<1>(ts);
+        if (t0 < closest_t && min_t < t0 && t0 < max_t) {
+            closest_t = t0;
             closest_sphere = spheres[i];
         }
-        if (ts[1] < closest_t && min_t < ts[1] && ts[1] < max_t) {
-            closest_t = ts[1];
+        if (t1 < closest_t && min_t < t1 && t1 < max_t) {
+            closest_t = t1;
             closest_sphere = spheres[i];
         }
     }
 
-    if (closest_t == INF) {
+    if (closest_t == INFINITY) {
         return BACKGROUND_COLOR;
     }
 
-    uint8_t* color = closest_sphere.color;
+    rgb color = closest_sphere.color;
     return color;
 }
 
@@ -151,7 +145,7 @@ int32_t main() {
     uint8_t data[width * height][3];
 
     // Define camera settings
-    float camera[3] = {0, 0, 0};
+    float3 camera = {0, 0, 0};
 
     // Define scene
     Sphere spheres[3] = {
@@ -160,12 +154,11 @@ int32_t main() {
         Sphere({-2.0f, 0, 4.0f}, 1.0f, {0, 255, 0})
     };
 
-    uint8_t color[3] = {255, 255, 255};
     for (int32_t x = -width / 2; x < width / 2; x++) {
         for (int32_t y = -height / 2; y < height / 2; y++)
         {
-            float* direction = canvas_to_viewport(x, y, width, height);
-            const uint8_t* color = trace_ray(camera, direction, 1.0f, INF, spheres, 3);
+            float3 direction = canvas_to_viewport(x, y, width, height);
+            rgb color = trace_ray(camera, direction, 1.0f, INFINITY, spheres, 3);
             put_pixel(data, width, height, x, y, color);
         }
     }
